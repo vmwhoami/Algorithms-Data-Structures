2ea2835636382dd4fe05bce46961fa2209aa6187
vmwhoami <vitalie.m@travod.com>
2022-02-21 23:48:18 +0200

    Finish with the coding challenge

--- lib/TwoNumsSum.rb
+++ lib/TwoNumsSum.rb
@@ -4,12 +4,12 @@ def TwoNumsSum(arr,target)
   h = {}
   arr.each_with_index do |num,index|
     return [target-num,num] if h[num]
-       
-    res = target - num
-    h[res] = true
+
+    h[target - num] = true
   end
- 
+  'No valid pair exists'
 end
 
 p TwoNumsSum([3, 5, -4, 8, 11, 1, -1, 6],10) #=> [11, -1] 
-
+p TwoNumsSum([0, -1, 2, -3, 1],-2) #=> [-3, 1]
+p TwoNumsSum([1, -2, 1, 0, 5],0) #=> No valid pair exists
