903f6c80548763dc4d674239244fdcd7b8b9062d
vmwhoami <vitalie.m@travod.com>
2022-02-21 23:11:36 +0200

    Set up two nums sum method

---
+++ lib/TwoNumsSum.rb
@@ -0,0 +1,11 @@
+# numbers in the array are distinct
+# return first two numbers that return the target
+def TwoNumsSum(arr,target)
+  h = {}
+  arr.each do |index,num|
+
+  end
+end
+
+p TwoNumsSum([1,2,3,4,5,6,7,8,9,10],15)
+
