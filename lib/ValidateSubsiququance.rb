8c209f7a332490dd5bd1a060499817844c7cf108
vmwhoami <vitalie.m@travod.com>
2022-02-22 22:37:53 +0200

    Solve validateSubsiquence

--- lib/ValidateSubsiququance.rb
+++ lib/ValidateSubsiququance.rb
@@ -1,5 +1,11 @@
 def validate_subsiquance(arr,subsequence)
-   
+  arr_index = 0
+  seq_index = 0
+  while arr_index < arr.size && seq_index < arr.size
+    seq_index += 1 if subsequence[seq_index] == arr[arr_index]
+    arr_index += 1
+  end
+  seq_index == subsequence.size
 end
 
-p validate_subsiquance([ 5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 10]) #=> true
+p validate_subsiquance([ 5, 1, 22, 25, 6, -1, 8, 10],[1, 6, -1, 10]) #=> true 5 1
