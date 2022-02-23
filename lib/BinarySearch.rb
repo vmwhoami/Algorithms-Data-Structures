8927c9863b6de6118f5e723ded1a279a9ac66c89
vmwhoami <vitalie.m@travod.com>
2022-02-24 00:07:28 +0200

    Add a guard clause to return -1 if no number was found

--- lib/BinarySearch.rb
+++ lib/BinarySearch.rb
@@ -2,6 +2,8 @@
 # If it is sorted there is an indication that binary search can be used
 def binary_search(arr, target)
   pointer = arr.size / 2
+  return -1 if arr.size.zero?
+
   return arr[pointer] if arr[pointer] == target
 
   if arr[pointer] > target
@@ -11,4 +13,9 @@ def binary_search(arr, target)
   end
 end
 
-p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33)
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33) #=> 33
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 72) #=> 72
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], -1) #=> -1
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 100) #=> -1
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 0) #=> 0
+p binary_search([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 1) #=> 1
