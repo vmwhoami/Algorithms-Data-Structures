7227e0c3055c3b653c171c6a9773e62c54783d34
vmwhoami <vitalie.m@travod.com>
2022-03-14 22:01:58 +0200

    Write out the basic requirments

--- lib/LinkedLists/linked_list_construction.rb
+++ lib/LinkedLists/linked_list_construction.rb
@@ -13,12 +13,36 @@ class LinkedList
   attr_accessor :node
   def initialize(node:)
     @node = node
+    add_node
+  end
+
+  def set_head
+   self.node.head = self.node if self.node.head.nil?
+  end
+
+  def remove_nodes_with(value)
+  end
+
+  def insert_node_at(position)
   end
 
   
+
+  def set_tail
+  end
   
+  def search_node
+  end
+
+  def remove_node
+
+
+  def add_node
+    self.set_head if self.node.head.nil?
+  end
 
 end
 
 
 list = LinkedList.new(node: Node.new(value: "the value"))
+ p list
